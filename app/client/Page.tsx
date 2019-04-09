87d385663f1613da9c10381c8f5a363ae37fadcd
Trish Ta <trish.ta@shopify.com>
2019-04-09 16:22:39 -0400

    Use react-router to handle app redirect actions Also keep the browser history up to date

--- app/client/Page.tsx
+++ app/client/Page.tsx
@@ -1,25 +1,19 @@
 import * as React from 'react';
 import { RouterProps } from 'react-router';
-import createApp, { LifecycleHook } from '@shopify/app-bridge';
-import { TitleBar } from '@shopify/app-bridge/actions';
+import { ClientApplication } from '@shopify/app-bridge';
+import { TitleBar, Redirect } from '@shopify/app-bridge/actions';
 
 interface Props extends RouterProps {
   content: string;
+  app: ClientApplication<{}>;
 }
 
 export default class Page extends React.Component<Props, never> {
   componentDidMount() {
-    const { history } = this.props;
+    const { app, history } = this.props;
 
-    const app = createApp({
-      apiKey: 'app_bridge_key',
-      shopOrigin: 'shop1.myshopify.io',
-    });
-
-    app.hooks.set(LifecycleHook.RedirectAppAction, () => {
-      return ({ path }) => {
-        history.replace(path);
-      };
+    app.subscribe(Redirect.ActionType.APP, ({ path }) => {
+      history.replace(path);
     });
 
     TitleBar.create(app, { title: this.props.content });
--- app/client/index.tsx
+++ app/client/index.tsx
@@ -1,66 +1,92 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-
+import createApp from '@shopify/app-bridge';
+import { History } from '@shopify/app-bridge/actions';
 import Page from './Page';
 
+const app = createApp({
+  apiKey: 'app_bridge_key',
+  shopOrigin: 'shop1.myshopify.io',
+});
+
 function Index(props: any) {
-  return <Page content="" {...props} />;
+  return <Page content="" {...props} app={app} />;
 }
 
 function Alpaca(props: any) {
-  return <Page content="Alpaca" {...props} />;
+  return <Page content="Alpaca" {...props} app={app} />;
 }
 
 function Bear(props: any) {
-  return <Page content="Bear" {...props} />;
+  return <Page content="Bear" {...props} app={app} />;
 }
 
 function Cat(props: any) {
-  return <Page content="Cat" {...props} />;
+  return <Page content="Cat" {...props} app={app} />;
 }
 
 function Dolphin(props: any) {
-  return <Page content="Dolphin" {...props} />;
+  return <Page content="Dolphin" {...props} app={app} />;
 }
 
 function Settings(props: any) {
-  return <Page content="Settings" {...props} />;
+  return <Page content="Settings" {...props} app={app} />;
+}
+
+function LinkWrapper({ path, content }: any) {
+  const historyAction = History.create(app);
+
+  function updateHistory() {
+    historyAction.dispatch(History.Action.REPLACE, path);
+  }
+
+  return (
+    <Link to={path} onClick={updateHistory}>
+      {content}
+    </Link>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <LinkWrapper path="/app" content="Index" />
+          </li>
+          <li>
+            <LinkWrapper path="/alpaca" content="Alpaca" />
+          </li>
+          <li>
+            <LinkWrapper path="/bear" content="Bear" />
+          </li>
+          <li>
+            <LinkWrapper path="/cat" content="Cat" />
+          </li>
+          <li>
+            <LinkWrapper path="/dolphin" content="Dolphin" />
+          </li>
+          <li>
+            <LinkWrapper path="/app/settings" content="Settings" />
+          </li>
+        </ul>
+      </nav>
+      <Route path="/app" exact component={Index} />
+      <Route path="/alpaca" component={Alpaca} />
+      <Route path="/bear" component={Bear} />
+      <Route path="/cat" component={Cat} />
+      <Route path="/dolphin" component={Dolphin} />
+      <Route path="/app/settings" component={Settings} />
+    </div>
+  );
 }
 
 export default function AppRouter() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/app">Index</Link>
-            </li>
-            <li>
-              <Link to="/alpaca">Alpaca</Link>
-            </li>
-            <li>
-              <Link to="/bear">Bear</Link>
-            </li>
-            <li>
-              <Link to="/cat">Cat</Link>
-            </li>
-            <li>
-              <Link to="/dolphin">Dolphin</Link>
-            </li>
-            <li>
-              <Link to="/app/settings">Settings</Link>
-            </li>
-          </ul>
-        </nav>
-        <Route path="/app" exact component={Index} />
-        <Route path="/alpaca" component={Alpaca} />
-        <Route path="/bear" component={Bear} />
-        <Route path="/cat" component={Cat} />
-        <Route path="/dolphin" component={Dolphin} />
-        <Route path="/app/settings" component={Settings} />
-      </div>
+      <App />
     </Router>
   );
 }
